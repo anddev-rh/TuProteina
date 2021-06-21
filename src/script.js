75134ad0f8a04d532f13db43eadbe5bd0218503c
Andres r hernandez <frodriguezh@unal.edu.co>
2021-06-21 17:52:47 -0500

    implementando calculadora con if y empezando con la sección de resultados.}

--- src/script.js
+++ src/script.js
@@ -11,17 +11,120 @@ const field3 = document.querySelector('.calculator-field--3')
 const field4 = document.querySelector('.calculator-field--4')
 
 
-const hideShow = (hide, show) => {
-  hide.classList.add('hide')
-  show.classList.remove('hide')
+const radio1 = document.getElementById('radio1')
+const radio2 = document.getElementById('radio2')
+const radio3 = document.getElementById('radio3')
+const radio4 = document.getElementById('radio4')
+
+const age = document.getElementById('age')
+const weight = document.getElementById('weight')
+
+let user = {
+  pregnantWoman: false,
+  age: Number,
+  exercise: false,
+  weight: Number,
+
+}
+
+//console.log(radioInput.value);
+
+const getAnswer = () => {
+  field0.classList.add('hide')
+  field1.classList.remove('hide')
+  
+  btn1.addEventListener('click', function(){
+    if(radio1.checked){
+      user.pregnantWoman = true
+      getWeight()
+    } if (radio2.checked) {
+      getAge()
+    }
+  })
+}
+
+const getWeight = () => {
+  field1.classList.add('hide')
+  field3.classList.add('hide')
+  field4.classList.remove('hide')
+  
+  let grams
+  let resultText = document.querySelector('.result-text')
+
+  btn4.addEventListener('click', function() {
+    if(user.pregnantWoman === true) {
+      grams = 1.5
+      let result = grams * parseFloat(weight.value)
+      resultText.textContent = `Debes consumir ${result} gramos de proteina al día`
+      //console.log(`Debes consumir ${result} gramos de proteina al día`);
+    } else if (user.age <= 5) {
+      grams = 2
+      let result = grams * parseFloat(weight.value)
+      resultText.textContent = `Debes consumir ${result} gramos de proteina al día`
+      //console.log(`Debes consumir ${result} gramos de proteina al día`);
+    } else if (user.age > 5 && user.age<= 18) {
+      grams = 1.5
+      let result = grams * parseFloat(weight.value)
+      resultText.textContent = `Debes consumir ${result} gramos de proteina al día`
+      //console.log(`Debes consumir ${result} gramos de proteina al día`);
+    } else if (user.age >= 50){
+      grams = 1.5
+      let result = grams * parseFloat(weight.value)
+      resultText.textContent = `Debes consumir ${result} gramos de proteina al día`
+      //console.log(`Debes consumir ${result} gramos de proteina al día.`);
+    } else if (user.age > 18 && user.age < 50){
+      if(user.exercise == true) {
+        grams = 1.4
+        grams2 = 1.7
+        let result = grams * parseFloat(weight.value)
+        let result2 = grams2 * parseFloat(weight.value)
+        resultText.textContent = `Debes consumir entre ${result} y ${result2} gramos de proteina al día.`
+        //console.log(`Debes consumir entre ${result} y ${result2} gramos de proteina al día.`);
+      } else {
+        grams = 1
+        let result = grams * parseFloat(weight.value)
+        resultText.textContent = `Debes consumir ${result} gramos de proteina al día ya que no haces ejercicio, sin embargo deberias empezar a hacerlo si quieres cuidar mejor tu salud.`
+        //console.log(`Debes consumir ${result} gramos de proteina al día ya que no haces ejercicio, sin embargo deberias empezar a hacerlo si quieres cuidar mejor tu salud.`);
+      }
+    }
+
+    let calculatorResult = document.querySelector('.calculator-result')
+    let advices = document.querySelector('.advices-container')
+
+    calculatorResult.classList.remove('hide')
+    advices.classList.remove('hide')
+  })
+}
+
+const getAge = () => {
+  field1.classList.add('hide')
+  field2.classList.remove('hide')
+  btn2.addEventListener('click', function(){
+    user.age = parseInt(age.value)
+    haveExercise()    
+  })
+}
+
+const haveExercise = () => {
+  field2.classList.add('hide')
+  field3.classList.remove('hide')
+  btn3.addEventListener('click', function(){
+    if(radio3.checked) {
+      user.exercise = true
+    }
+    if(radio4.checked) {
+      user.exercise = false
+    }
+    getWeight()
+  })
 }
 
 
 
-btnStart.addEventListener('click', function() {hideShow(field0, field1)})
-btn1.addEventListener('click', function() {hideShow(field1, field2)})
-btn2.addEventListener('click', function() {hideShow(field2, field3)})
-btn3.addEventListener('click', function() {hideShow(field3, field4)})
+btnStart.addEventListener('click', getAnswer)
+//btn1.addEventListener('click', function() {hideShow(field1, field2)})
+//btn2.addEventListener('click', function() {hideShow(field2, field3)})
+//btn3.addEventListener('click', function() {hideShow(field3, field4)})
 //btn4.addEventListener('click', function() {hideShow(field1, field2)})
 
 
